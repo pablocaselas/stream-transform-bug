5020200971c976cdf07e69d2e545109840ed56ac
Pablo Caselas <pablo.caselas@solvver.com>
2013-12-19 16:58:57 +0100

    fixed bug. Now implementing done() in each _flush method

--- test/streamBugTest.js
+++ test/streamBugTest.js
@@ -6,9 +6,9 @@ var reader=new readerFactory(function(){return {a:1}});
 var filt1=new filterFactory(function(data){return data});
 var filt2=new filterFactory(function(data){return data});
 var filt3=new filterFactory(function(data){return data});
-filt1._flush=function(){console.log("FLUSH filt1")}
-filt2._flush=function(){console.log("FLUSH filt2")}
-filt3._flush=function(){console.log("FLUSH filt3")}
+filt1._flush=function(done){console.log("FLUSH filt1");done()}
+filt2._flush=function(done){console.log("FLUSH filt2");done()}
+filt3._flush=function(done){console.log("FLUSH filt3");done()}
 reader.pipe(filt1).pipe(filt2).pipe(filt3).pipe(new devnull());
 
 setTimeout(function(){reader.destroy()}, 1000);
